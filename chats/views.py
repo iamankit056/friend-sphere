41ad50be542b2fb8d178e9422b9a96bcac85b24c
Ankit <iamankit056@gmail.com>
2023-04-20 19:36:25 +0530

    friend-sphere

--- chats/views.py
+++ chats/views.py
@@ -1,41 +1,17 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
+from django.contrib.auth.models import User
 from users.models import Profile
-from chats.models import ChatMessage
-from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from chats.serializers import ChatMessageSerialzer
 
 # Create your views here.
 def home(request):
-    return render(request, 'chats/home.html')
+    users = User.objects.all()
+    profiles = User.objects.all()
+
+    context = {
+        'users': users,
+        'profiles': profiles
+    }
+
+    return render(request, 'chats/home.html', context)
 
 
-@api_view(['GET', 'POST', 'DELETE'])
-def chat_messages(request, user_id):
-
-    # /messages/user_id => GET
-    if request.method == 'GET':
-        chat_messages = ChatMessage.objects.filter(msg_sender=request.user.pk, msg_reciver=user_id)
-        serialize_chat_messages = ChatMessageSerialzer(chat_messages, many=True)
-
-        return Response(serialize_chat_messages.data, status=status.HTTP_200_OK)
-    
-    # /messages/user_id => POST
-    if request.method == 'POST':
-        serialize_chat_messages = ChatMessageSerialzer(data=request.data)
-        if serialize_chat_messages.is_valid():
-            serialize_chat_messages.save()
-            return Response(serialize_chat_messages.data, status=status.HTTP_201_CREATED)
-        return Response(status=status.HTTP_400_BAD_REQUEST)  
-
-    # /messages/user_id => DELETE
-    if request.method == 'DELETE':
-        try:
-            chat_messages = ChatMessage.objects.get(pk = request.data['pk'])
-            chat_messages.delete()
-            return Response(status=status.HTTP_204_NO_CONTENT)
-        except:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-        
-    return Response(status=status.HTTP_503_SERVICE_UNAVAILABLE)
--- users/views.py
+++ users/views.py
@@ -1,10 +1,6 @@
 from django.shortcuts import render
-from django.contrib.auth.models import User
-from rest_framework import status
-from rest_framework.response import Response
-from rest_framework.decorators import api_view
 from users.models import Profile
-from users.serializers import UserSerializer, ProfileSerializer
+from django.contrib.auth.models import User
 
 # Create your views here.
 def signup(request):
@@ -14,18 +10,4 @@ def signup(request):
 def signin(request):
     return render(request, 'users/login.html')
 
-
-@api_view(['GET'])
-def get_users(request):
-    users = User.objects.all()
-    serialize_user = UserSerializer(users, many=True)    
-    profiles = Profile.objects.all()
-    serialize_profile = ProfileSerializer(profiles, many=True)
-
-    json_data = {
-        'users': serialize_user.data,
-        'profiles': serialize_profile.data
-    }
-
-    return Response(json_data, status=status.HTTP_200_OK)
 
