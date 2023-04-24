f5a3fbbcc28563dd8cca3d035ca5a5594ae9e0cd
Ankit <iamankit056@gmail.com>
2023-04-24 21:06:14 +0530

    minner changes

--- chats/views.py
+++ chats/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render
 from chats.models import ChatMessage
-from users.models import Profile
+from django.contrib.auth.models import User
 from chats.serializers import ChatMessageSerialzer
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from django.db.models import Q
 
 # Create your views here.
 def home(request):
-    users_profile = Profile.objects.all()
+    users = User.objects.all()
     context = {
-        'users_profile': users_profile
+        'users': users
     }
     return render(request, 'chats/home.html', context)
 
@@ -18,15 +19,11 @@ def home(request):
 @api_view(['GET'])
 def get_chats(request, receiver_id):
     if request.method == 'GET':
-        sender_chats = ChatMessage.objects.filter(msg_sender=request.user.id, msg_receiver=receiver_id)
-        reciver_chats = ChatMessage.objects.filter(msg_sender=receiver_id, msg_receiver=request.user.id)
-        serialize_sender_chats = ChatMessageSerialzer(sender_chats, many=True)
-        serialize_reciver_chats = ChatMessageSerialzer(reciver_chats, many=True)
-        json_data = {
-            'sender_messages': serialize_sender_chats.data,
-            'serialize_reciver_chats': serialize_reciver_chats.data
-        }
-        return Response(json_data, status=status.HTTP_200_OK)
+        chats = ChatMessage.objects.filter(
+            Q(msg_sender=request.user.id) & Q(msg_receiver=receiver_id) |
+            Q(msg_sender=receiver_id) & Q(msg_receiver=request.user.id))
+        serialize_chats = ChatMessageSerialzer(chats, many=True)
+        return Response(serialize_chats.data, status=status.HTTP_200_OK)
     return Response({'error': 'something went wrong, failed to fetch data!'}, status=status.HTTP_408_REQUEST_TIMEOUT)
 
 
--- static/js/chats/home.js
+++ static/js/chats/home.js
@@ -2,12 +2,14 @@ let active_user
 const time_for_makeing_chat_request = 2000
 
 function ActivateChattingForReceiver(receiver_id) {
-    if (!active_user)
-        clearInterval(active_user)
+    // if (!active_user)
+    //     clearInterval(active_user)
 
-    active_user = setInterval(() => {
-        LoadChats(receiver_id)
-    }, time_for_makeing_chat_request)
+    // active_user = setInterval(() => {
+    //     LoadChats(receiver_id)
+    // }, time_for_makeing_chat_request)
+
+    LoadChats(receiver_id)
 }
 
 function LoadChats(receiver_id) 
@@ -16,27 +18,23 @@ function LoadChats(receiver_id)
         type: 'GET',
         url: 'http://localhost:9000/receiver/' + receiver_id + '/chats',
         success: function(chats, status) {
-            if(status == 'success') {
-                // load sender chats
-                chats.sender.forEach(chat => {
-                    $('#sender_chats').append(ChatRowHtml(chat))
-                });
-                // load reveiver chats
-                chats.reciver.forEach(chat => {
-                    $('#receiver_chats').append(ChatRowHtml(chat))
+            if(status == 'success') 
+            {
+                chats.forEach(chat => {
+                    if(chat.msg_sender == receiver_id)
+                        $('#my-chats').append(ChatRowHtml(chat, 'receiver'));
+                    else 
+                        $('#my-chats').append(ChatRowHtml(chat, 'sender'));
+
                 });
             }
         }
     })
 }
 
-function ChatRowHtml(chat) {
-    return ('<tr>' + 
-        '<td>' + chat.id + '</td>' +
-        '<td>' + chat.text + '</td>' +
-        '<td>' + chat.file + '</td>' +
-        '<td>' + chat.msg_sender + '</td>' +
-        '<td>' + chat.msg_receiver + '</td>' +
-        '<td>' + chat.is_read + '</td>' +
-    '</tr>')
+function ChatRowHtml(chat, className='sender') {
+    return (
+        '<div class="' + className + '">' +
+            '<p>' + chat.text + '</p>' +
+        '</div>')
 }
